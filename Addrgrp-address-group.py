2b8d5c44bea36797072b9a48ed064a5c1ba2b427
0tax03 <84558385+0tax03@users.noreply.github.com>
2021-07-08 20:10:00 -0300

    Update Addrgrp-address-group.py

--- Addrgrp-address-group.py
+++ Addrgrp-address-group.py
@@ -7,7 +7,7 @@ Mode of use:
 
 1- Insert in the variable "member" the name of each object respecting the python syntax.
 2- Insert the address group in the "grp" variable
-3- After that save the script, run, take your output and just copy it to fortigate the CLI.
+3- After that save the script, run, take your output and just copy it to fortigate through the CLI.
 '''
 
 member = ('teste','teste')
