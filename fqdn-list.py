2e73b3f2c3ad1ad63cde2f92de235c7db311bdeb
Mateus Pantoja <84558385+0tax03@users.noreply.github.com>
2022-06-23 17:31:08 -0300

    Update fqdn-list.py

--- fqdn-list.py
+++ fqdn-list.py
@@ -1,15 +1,3 @@
-'''
-fqdn.py
-
-Script to create fqdn objects.
-
-Mode of use:
-
-1- Insert in the variable "fqdn" the name of each object respecting the python syntax.
-2- After that save the script, run, take your output and just copy it to fortigate through the CLI.
-
-'''
-
 fqdn = ('teste','teste')
 a = '"'
 print("config firewall address")
